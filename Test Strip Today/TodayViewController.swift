85fd1ea6a784149ad2842744d28130df5ceef42a
Keaton Burleson <keaton.burleson@me.com>
2016-04-28 17:35:02 -0500

    Finalized everything (ready for submission)
    Wish me luck :)

--- Test Strip Today/TodayViewController.swift
+++ Test Strip Today/TodayViewController.swift
@@ -10,65 +10,157 @@ import UIKit
 import NotificationCenter
 import QuartzCore
 import Foundation
+
 class TodayViewController: UITableViewController, NCWidgetProviding {
     
     @IBOutlet var high: UILabel?
     @IBOutlet var low: UILabel?
     @IBOutlet var average: UILabel?
+    
+    var mealsArray: NSMutableArray?
+    
      let defaults = NSUserDefaults(suiteName: "group.com.128keaton.test-strip")
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.preferredContentSize = CGSizeMake(self.preferredContentSize.width, 320);
-        for view in self.tableView.subviews {
-            view.backgroundColor = UIColor.clearColor()
+        if objectAlreadyExist("meals")  {
+            mealsArray = NSMutableArray()
+            for i in 0..<4 {
+                    mealsArray?.addObject((defaults!.objectForKey("meals")?.mutableCopy() as? NSMutableArray?)!![i])
+            }
+            self.preferredContentSize = CGSizeMake(self.preferredContentSize.width, CGFloat(4 * 150));
+        } else {
+            self.preferredContentSize = CGSizeMake(self.preferredContentSize.width, CGFloat(44));
+            let backgroundView = UILabel(frame: self.view.frame)
+            backgroundView.text = "No readings"
+            backgroundView.textAlignment = .Center
+            backgroundView.textColor = UIColor.whiteColor()
+            self.tableView.backgroundView = backgroundView
+            
         }
-        if ((defaults?.doubleForKey("highscore")) != nil) {
-            high?.text = "\(defaults!.doubleForKey("highscore")) mg/dL"
-        }else{
-            high?.text = "No data"
-        }
+       
         
-        if ((defaults?.doubleForKey("lowscore")) != nil) {
-            low?.text = "\(Int(round(defaults!.doubleForKey("lowscore")))) mg/dL"
-        }else{
-            low?.text = "No data"
-        }
-        if ((defaults?.doubleForKey("average")) != nil) {
-            average?.text = "\(round(defaults!.doubleForKey("average"))) mg/dL"
-        }else{
-            average?.text = "No data"
-        }
-        
-        high?.backgroundColor = UIColor.redColor()
-        high?.layer.cornerRadius = 5
-        high?.layer.masksToBounds = true
-        
-     
-        low?.backgroundColor = UIColor(colorLiteralRed: 0.9843, green: 0.8235, blue: 0.0353, alpha: 1.0)
-        low?.layer.cornerRadius = 5
-        low?.layer.masksToBounds = true
-        
-        average?.backgroundColor = self.view.tintColor
-        average?.layer.cornerRadius = 5
-        average?.layer.masksToBounds = true
         
         
         // Do any additional setup after loading the view from its nib.
     }
-    @IBAction func openHome(){
-        self.extensionContext!.openURL(NSURL(string: "test-strip://")!, completionHandler: nil)
+    
+    func objectAlreadyExist(key: String) -> Bool {
+        return defaults!.objectForKey(key) != nil
     }
+    override func viewDidAppear(animated: Bool) {
+        self.tableView.reloadData()
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = self.tableView!.dequeueReusableCellWithIdentifier("cell") as! MealCell
+        let type = mealsArray![indexPath.row]["type"] as! String
+        //TODO - clean this monstrosity up
+        if(mealsArray![indexPath.row]["bloodGlucose"] is String){
+            cell.bloodGlucose?.text = "\(mealsArray![indexPath.row]["bloodGlucose"] as! String)\nmg/dL"
+        }else{
+            cell.bloodGlucose?.text = "\(mealsArray![indexPath.row]["bloodGlucose"] as! Double)\nmg/dL"
+        }
+        if(mealsArray![indexPath.row]["carbs"] is String){
+            cell.carbs?.text = "\(mealsArray![indexPath.row]["carbs"] as! String)\ncarbs"
+        }else{
+            cell.carbs?.text = "\(mealsArray![indexPath.row]["carbs"] as! Double)\ncarbs"
+        }
+        if(mealsArray![indexPath.row]["insulin"] is String){
+            cell.insulin?.text = "\(mealsArray![indexPath.row]["insulin"] as! String)\nunits"
+        }else{
+            cell.insulin?.text = "\(mealsArray![indexPath.row]["insulin"] as! Double)\nunits"
+        }
+        
+        
+        
+        
+        
+        cell.type?.text = mealsArray![indexPath.row]["type"] as? String
+        cell.backgroundColor = UIColor.blackColor()
+        cell.time?.text = self.getTime(indexPath)
+        
+        cell.bloodGlucose?.layer.cornerRadius = 5
+        cell.bloodGlucose?.clipsToBounds = true
+        cell.bloodGlucose?.backgroundColor = UIColor.whiteColor()
+        cell.bloodGlucose?.textColor = UIColor.blackColor()
+        
+        cell.carbs?.layer.cornerRadius = 5
+        cell.carbs?.clipsToBounds = true
+        cell.carbs?.backgroundColor = self.view.tintColor
+        
+        cell.insulin?.layer.cornerRadius = 5
+        cell.insulin?.clipsToBounds = true
+        cell.insulin?.backgroundColor = UIColor.greenColor()
+        
+        cell.time?.textColor = UIColor.whiteColor()
+        cell.time?.clipsToBounds = true
+        cell.time?.layer.cornerRadius = 5
+        
+        
+        if(type == "Full Meal"){
+            cell.mealType?.image = UIImage.init(named: "Meal.png")
+        }else if(type == "Insulin Correction"){
+            cell.mealType?.image = UIImage.init(named: "Adjustment.png")
+        }else if(type == "Long Lasting"){
+            cell.mealType?.image = UIImage.init(named: "24H.png")
+        }
+        return cell
+    }
+    func getTime(indexPath: NSIndexPath) -> String{
+        let dictionary = mealsArray![indexPath.row] as! NSMutableDictionary
+        if let exists = dictionary["date"]{
+            let formatter = NSDateFormatter()
+            formatter.dateStyle = .ShortStyle
+            formatter.timeStyle = .ShortStyle
+            return formatter.stringFromDate(exists as! NSDate)
+        }else{
+            return "No time recorded"
+        }
+    }
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+        
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if objectAlreadyExist("meals")  {
+            return 4
+        }else{
+            return 0
+        }
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        self.tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        self.extensionContext!.openURL(NSURL(string: "test-strip://")!, completionHandler: nil)
+        
+    }
+ 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
     func widgetPerformUpdateWithCompletionHandler(completionHandler: ((NCUpdateResult) -> Void)) {
-        // Perform any setup necessary in order to update the view.
-
-        // If an error is encountered, use NCUpdateResult.Failed
-        // If there's no update required, use NCUpdateResult.NoData
-        // If there's an update, use NCUpdateResult.NewData
+        if objectAlreadyExist("meals")  {
+            mealsArray = NSMutableArray()
+            for i in 0..<4 {
+                mealsArray?.addObject((defaults!.objectForKey("meals")?.mutableCopy() as? NSMutableArray?)!![i])
+            }
+            self.preferredContentSize = CGSizeMake(self.preferredContentSize.width, CGFloat(4 * 150));
+        } else {
+            self.preferredContentSize = CGSizeMake(self.preferredContentSize.width, CGFloat(44));
+            let backgroundView = UILabel(frame: self.view.frame)
+            backgroundView.text = "No readings"
+            backgroundView.textAlignment = .Center
+            backgroundView.textColor = UIColor.whiteColor()
+            self.tableView.backgroundView = backgroundView
+            
+        }
+        
+        
+        
 
         completionHandler(NCUpdateResult.NewData)
     }
