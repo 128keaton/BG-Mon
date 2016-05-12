b1402d72b6f89ad0af487d8b9f80a69aa87668a5
Keaton Burleson <keaton.burleson@me.com>
2016-05-12 00:03:41 -0500

    it works good enough for now

--- Bg-Mon/EmbedGauges.swift
+++ Bg-Mon/EmbedGauges.swift
@@ -19,5 +19,27 @@ class EmbedGauges: UITableViewController {
     
     @IBOutlet var doseLabel: UILabel?
     @IBOutlet var doseGauge: Gauge?
+    
+    
+    @IBOutlet var bottomCell: UITableViewCell?
+    @IBOutlet var topCell: UITableViewCell?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        topCell?.frame = CGRectMake(0, 0, self.view.frame.width, 110)
+        bottomCell?.frame = CGRectMake(0, 0, self.view.frame.width, 119)
+    }
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        if indexPath.row == 0 {
+            return 110
+        }else{
+            return 119
+        }
+    }
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
 }
 
